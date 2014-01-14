9c67bf29cdfe447958047757de1dc0d5487e4524
derisdjumadi <absdroid04@gmail.com>
2014-01-14 16:07:43 +0800

    controllers/Home

---
+++ Home.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace controllers;
+use lib\MVC\Controller\BaseController;
+
+class Home extends BaseController {
+    protected function Index() {
+        $viewModel = "DATA";
+        $this->RenderView($viewModel);
+    }
+}
