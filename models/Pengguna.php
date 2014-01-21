3934d35ec7724571b13fafdce0b1292b99a5c605
derisdjumadi <absdroid04@gmail.com>
2014-01-21 18:52:09 +0800

    Create Pengguna.php
    Class Pengguna ada didalam folder models yang ada dalam diretori utama

---
+++ models/Pengguna.php
@@ -0,0 +1,23 @@
+<?php
+namespace models;
+use lib\MVC\Model\BaseModel;
+
+class Pengguna extends BaseModel{
+    public $nama;
+    public $email;
+    
+    public function __construct($nama, $email) {
+        $this->nama = $nama;
+        $this->email = $email;
+    }
+    public static function getPengguna(){
+        $query = self::getDB()->prepare("Select * from 'pengguna'");
+        $query->execute();
+        
+        $result = array();
+        while ($row = $query->fetch()){
+            array_push($result, new Pengguna($row["nama"], $row["email"]));
+        }
+        return $result;
+    }
+}
